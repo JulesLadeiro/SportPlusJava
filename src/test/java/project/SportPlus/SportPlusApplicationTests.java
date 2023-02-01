7f233935d041bc78698dbda41d16d39ebce524c5
Jules Sorensen <julessorens@gmail.com>
2023-02-01 10:33:16 +0100

    Initial commit

---
+++ src/test/java/project/SportPlus/SportPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package project.SportPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
