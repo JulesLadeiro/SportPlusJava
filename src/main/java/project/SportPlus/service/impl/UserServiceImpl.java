591ec497e3e7608762a43bf7459145e1d081421d
Jules Sorensen <julessorens@gmail.com>
2023-02-05 11:09:27 +0100

    Update user service

--- src/main/java/project/SportPlus/service/impl/UserServiceImpl.java
+++ src/main/java/project/SportPlus/service/impl/UserServiceImpl.java
@@ -25,17 +25,17 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void createUser(User demo) {
-        userRepository.save(demo);
+    public void createUser(User user) {
+        userRepository.save(user);
     }
 
     @Override
-    public void updateUser(User demo, Long id) {
-        User maDemo = this.getUserById(id);
-        maDemo.setUsername(demo.getUsername());
-        maDemo.setPassword(demo.getPassword());
-        maDemo.setRole(demo.getRole());
-        userRepository.save(maDemo);
+    public void updateUser(User user, Long id) {
+        User updatedUser = this.getUserById(id);
+        updatedUser.setUsername(user.getUsername());
+        updatedUser.setPassword(user.getPassword());
+        updatedUser.setRole(user.getRole());
+        userRepository.save(updatedUser);
     }
 
     @Override
